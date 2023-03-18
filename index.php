d9684770a5d74537e263d8662d77b12583ed5c90
jaybaybremner <j.bremner@ymail.com>
2023-03-18 14:18:10 +0000

    add video page

--- index.php
+++ index.php
@@ -17,47 +17,47 @@
     <link rel="icon" href="./assets/images/ukorea_logo_final.svg">
 </head>
 <body>
-    <header>
-        <div class="header-top">
-            <img src="./assets/images/ukorea_logo_final.svg" alt="UK/Korea 140 years logo" height="60" class="korea-uk-140-logo">
-            <nav>
-                <ul class="navigation-links">
-                    <li><a href="./homepage-korean.html">Ko</a></li>
-                    <li>|</li>
-                    <li><a href="./index.html">En</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href = "mailto: ukkorea140@gmail.com" class="contact-us-link">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-     
-        <div class="header-title-block">
+<header>
+    <div class="header-top">
+        <img src="./assets/images/ukorea_logo_final.svg" alt="UK/Korea 140 years logo" height="60" class="korea-uk-140-logo">
+        <nav>
+            <ul class="navigation-links">
+                <li><a href="./homepage-korean.html">Ko</a></li>
+                <li>|</li>
+                <li><a href="./index.html">En</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href = "mailto: ukkorea140@gmail.com" class="contact-us-link">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+ 
+    <div class="header-title-block">
+  
+        <h2>140 years of UK-Korea relations</h2>
+        <img src="./assets/images/header_uk_flag.svg" alt="UK flag icon" height="30" class="mobile-hide">
+        <img src="./assets/images/header_korea_flag.svg" alt="South Korean flag icon" class="mobile-hide" height="30">
       
-            <h2>140 years of UK-Korea relations</h2>
-            <img src="./assets/images/header_uk_flag.svg" alt="UK flag icon" height="30" class="mobile-hide">
-            <img src="./assets/images/header_korea_flag.svg" alt="South Korean flag icon" class="mobile-hide" height="30">
-          
-        </div>
-        <div class="header-50-50-container">
-       <div class="header-text-container">
-        <h3 class="text-korean">한영 수교 140주년 기념 행사</h3>
-        <img src="./assets/images/header_title.svg" alt="Share your story title" height="71" class="mobile-hide header-title-image">
-        <img src="./assets/images/mobile_header_title.svg" alt="Share your story title" height="130" class="desktop-hide header-title-image-mobile">
-        <p class="header-subtitle">Join our video contest to celebrate 140 years of friendship.</p>
-        <span class="red-label">Application window</span>
-        <div class="apply-container">
-        <p>27.02.2023 ~ 26.03.2023</p>  
-        <a href="https://form.jotform.com/ukorea140/application-form-for-uk-korea-140" target="_blank" class="apply-btn mobile-hide">Apply now!</a>
-        </div>
-        <p class="header-notice">*Open to residents of the United Kingdom or the Republic of Korea only</p>
     </div>
-    <div class="header-image-container">
-        <img src="./assets/images/header_image.svg" alt="2 youtube videos images with a South Korean and British flag" height="246" class="mobile-hide header-video-image">
+    <div class="header-50-50-container">
+   <div class="header-text-container">
+    <h3 class="text-korean">한영 수교 140주년 기념 행사</h3>
+    <img src="./assets/images/header_title.svg" alt="Share your story title" height="71" class="mobile-hide header-title-image">
+    <img src="./assets/images/mobile_header_title.svg" alt="Share your story title" height="130" class="desktop-hide header-title-image-mobile">
+    <p class="header-subtitle">Join our video contest to celebrate 140 years of friendship.</p>
+    <span class="red-label">Application window</span>
+    <div class="apply-container">
+    <p>27.02.2023 ~ 26.03.2023</p>  
+    <a href="https://form.jotform.com/ukorea140/application-form-for-uk-korea-140" target="_blank" class="apply-btn mobile-hide">Apply now!</a>
     </div>
-    </div>
-    <!--header 50/50 container end-->
-      
-    </header>
+    <p class="header-notice">*Open to residents of the United Kingdom or the Republic of Korea only</p>
+</div>
+<div class="header-image-container">
+    <img src="./assets/images/header_image.svg" alt="2 youtube videos images with a South Korean and British flag" height="246" class="mobile-hide header-video-image">
+</div>
+</div>
+<!--header 50/50 container end-->
+  
+</header>
     <main>
         <section class="description">
 
---
+++ video-playlist.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>video playlist page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="video-grid.css">
+</head>
+<body>
+    <!--video header-->
+    <header>
+        <div class="header-top">
+            <img src="./assets/images/ukorea_logo_final.svg" alt="UK/Korea 140 years logo" height="60" class="korea-uk-140-logo">
+            <nav>
+                <ul class="navigation-links">
+                    <li><a href="./homepage-korean.html">Ko</a></li>
+                    <li>|</li>
+                    <li><a href="./index.html">En</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href = "mailto: ukkorea140@gmail.com" class="contact-us-link">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+     
+        <div class="header-title-block">
+      
+            <h2>140 years of UK-Korea relations</h2>
+            <img src="./assets/images/header_uk_flag.svg" alt="UK flag icon" height="30" class="mobile-hide">
+            <img src="./assets/images/header_korea_flag.svg" alt="South Korean flag icon" class="mobile-hide" height="30">
+          
+        </div>
+        <div class="header-50-50-container">
+       <div class="header-text-container">
+        <h3 class="text-korean">한영 수교 140주년 기념 행사</h3>
+        <img src="./assets/images/header_title.svg" alt="Share your story title" height="71" class="mobile-hide header-title-image">
+        <img src="./assets/images/mobile_header_title.svg" alt="Share your story title" height="130" class="desktop-hide header-title-image-mobile">
+        <p class="header-subtitle">Join our video contest to celebrate 140 years of friendship.</p>
+        <span class="red-label">Judge period</span>
+        <div class="apply-container">
+        <p>27.02.2023 ~ 01.04.2023</p>  
+        </div>
+        <span class="red-label">Winner announcement</span>
+        <p>06.04.2023</p>  
+        <p class="header-notice">*Open to residents of the United Kingdom or the Republic of Korea only</p>
+
+    </div>
+    <div class="header-image-container">
+        <img src="./assets/images/header_image.svg" alt="2 youtube videos images with a South Korean and British flag" height="246" class="mobile-hide header-video-image">
+    </div>
+    </div>
+    <!--header 50/50 container end-->
+      
+    </header>
+    <!--video list-->
+    <main>
+        <div class="header-container">
+            <h2>Videos</h2>
+            <p>Send your like and leave your comments!</p>
+        </div>
+        <div class=video-grid-container>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5o5DBhiPzzs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/TcNQPCiB_wE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7S_tz1z_5bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+   
+    </main>
+   
+    <!--footer-->
+    <footer>
+
+<div class="korean-embassy-logo">
+    <div class="image-container">
+        <img src="./assets/images/korean embassy_logo.svg" alt="Republic of Korea embassy logo" height="100">
+    </div>
+    <div class="text-container">
+        <p>Embassy of the Republic of Korea</p>
+        <p class="text-korean">주영국 대한민국 대사관 </p>
+    </div>
+  </div>
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href = "mailto: ukorea140@gmail.com">Contact</a></li>
+        <li><a href="https://form.jotform.com/ukorea140/privacy-policy" target="_blank">Privacy policy</a></li>
+        <li><a href="./homepage-korean.html" class="language-link">Ko</a> | <a href="./index.html" class="language-link">En</a></li>
+    </ul>
+</nav>
+<ul class="social-media-footer">
+    <li><a href="https://www.instagram.com/ukorea140/?hl=en"><img src="./assets/images/instagram.svg" alt="facebook social icon" height="20"></a></li>
+    <li><a href="https://www.youtube.com/@ukorea140"><img src="./assets/images/youtube.svg" alt="facebook social icon" height="20"></a></li>
+    <li><a href="https://www.facebook.com/ukorea140"><img src="./assets/images/facebook_1.svg" alt="facebook social icon" height="20"></a></li>
+    <li><a href="https://www.twitter.com/ukorea140"><img src="./assets/images/twitter_1.svg" alt="facebook social icon" height="20"></a></li>
+   
+</ul>
+<p class="copyright-text">Copyright ukorea140 &copy; 2023</p>
+
+</footer>
+</body>
+</html>
